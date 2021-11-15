902d300024c2bbdb90a4455f5885cf592b92a7f5
손기연 <sonky740@minwise.co.kr>
2021-11-15 16:19:14 +0900

    init

---
+++ why-ts/app.js
@@ -0,0 +1,48 @@
+// api url
+var url = 'https://jsonplaceholder.typicode.com/users/1';
+
+// dom
+var username = document.querySelector('#username');
+var email = document.querySelector('#email');
+var address = document.querySelector('#address');
+
+// user data
+var user = {};
+
+/**
+ * @typedef {object} Address
+ * @property {string} street
+ * @property {string} email
+ */
+
+
+/**
+ * @typedef {object} User
+ * @property {string} name
+ * @property {string} email
+ * @property {Address} address
+ */
+
+/**
+ * @returns {Promise<User>}
+ */
+function fetchUser() {
+  return axios.get(url);
+}
+
+function startApp() {
+  fetchUser()
+    .then(function (response) {
+      user = response.data;
+      console.log(user);
+
+      username.innerText = user.name;
+      email.innerText = user.email;
+      address.innerText = user.address.street;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+startApp();
---
+++ why-ts/sample.js
@@ -0,0 +1,12 @@
+// @ts-check
+
+/**
+ * 
+ * @param {number} a 첫번째 숫자
+ * @param {number} b 두번째 숫자
+ */
+function sum(a, b) {
+    return a + b;
+}
+
+var result = sum(10, 20);
