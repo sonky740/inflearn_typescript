0f3495d971b5e12ab5565359f2a460f500343e14
sonky740 <sonky740@inpix.com>
2021-11-16 21:32:07 +0900

    .

--- class-note/1_type-basic.ts
+++ class-note/1_type-basic.ts
@@ -9,4 +9,17 @@ let num: number = 10;
 
 // TS 배열
 let arr: number[] = [1, 2, 3]; // literal
-let heroes: Array<string> = ['Capt', 'Thor', 'Hulk'];
+let heroes: Array<string> = ["Capt", "Thor", "Hulk"];
+
+// TS 튜플
+let address: [string, number] = ["gangnam", 1];
+
+// TS 객체
+let obj: object = {};
+let person: { name: string; age: number } = {
+  name: "capt",
+  age: 100,
+};
+
+// 진위값
+let show: boolean = true;
---
+++ class-note/function.js
@@ -0,0 +1,5 @@
+function sums(a, b) {
+    return a + b;
+}
+
+sums(10, 20, 30, 40, 50);
