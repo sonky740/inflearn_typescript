287350c028901f849ed8e9fca75a3daa156bda5a
손기연 <sonky740@minwise.co.kr>
2021-11-18 16:52:39 +0900

    2번째 플젝 전

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -6,13 +6,69 @@
 // logText<string>("하이");
 // logText<number>(10);
 
+// function logText(text: string | number) {
+//   console.log(text);
+//   return text;
+// }
 
-function logText(text: string | number) {
+// const a = logText('a');
+// logText(10);
+
+function logText<T>(text: T): T {
   console.log(text);
   return text;
 }
+// 제네릭을 쓰면 자동완성에서 이점이 있음.
+const str = logText<string>("abc");
+const login = logText<boolean>(true);
 
-const a = logText('a');
-logText(10);
+// 인터페이스에 제네릭을 선언하는 방법
+// interface Dropdown {
+//   value: string;
+//   selected: boolean;
+// }
+// const obj: Dropdown = { value: "abc", selected: false };
 
-// 제네릭의 장점좌 타입 추론에서의 이점
+interface Dropdown<T> {
+  value: T;
+  selected: boolean;
+}
+const obj: Dropdown<string> = { value: "abc", selected: false };
+
+// 제네릭의 타입 제한
+function logTextLength<T>(text: T[]): T[] {
+  console.log(text.length);
+  text.forEach(function (text) {
+    console.log(text);
+  });
+  return text;
+}
+logTextLength<string>(["hi", "abc"]);
+
+// 제네릭 타입 제한 2 - 정의된 타입 이용하기
+interface LengthType {
+  length: number;
+}
+
+function logTextLength2<T extends LengthType>(text: T): T {
+  console.log(text.length);
+  return text;
+}
+logTextLength2("a");
+
+// 제네릭 타입 제한 3 - keyof
+interface ShoppingItem {
+  name: string;
+  price: number;
+  stock: number;
+}
+
+function getShoppingItemOption<T extends keyof ShoppingItem>(itemOption: T): T {
+  return itemOption;
+}
+
+// getShoppingItemOption(10);
+// getShoppingItemOption<string>('a');
+getShoppingItemOption("name");
+getShoppingItemOption("price");
+getShoppingItemOption("stock");
