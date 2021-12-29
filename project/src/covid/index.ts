9df4124df66506504acc2e8bde3c9ff6a96ab494
손기연 <sonky740@minwise.co.kr>
2021-12-29 13:37:15 +0900

    typescript 변환

---
+++ project/src/covid/index.ts
@@ -0,0 +1,52 @@
+export enum CovidStatus {
+  Confirmed = 'confirmed',
+  Recoverd = 'recovered',
+  Deaths = 'deaths',
+}
+
+export interface Country {
+  Country: string;
+  CountryCode: string;
+  Date: string;
+  ID: string;
+  NewConfirmed: number;
+  NewDeaths: number;
+  NewRecovered: number;
+  Premium: object;
+  Slug: string;
+  TotalConfirmed: number;
+  TotalDeaths: number;
+  TotalRecovered: number;
+}
+
+interface Global {
+  Date: string;
+  NewConfirmed: number;
+  NewDeaths: number;
+  NewRecovered: number;
+  TotalConfirmed: number;
+  TotalDeaths: number;
+  TotalRecovered: number;
+}
+
+export interface CovidSummaryResponse {
+  Countries: Country[];
+  Date: string;
+  Global: Global;
+  Message: string;
+}
+
+export interface CountrySummaryInfo {
+  Cases: number;
+  City: string;
+  CityCode: string;
+  Country: string;
+  CountryCode: string;
+  Date: string;
+  Lat: string;
+  Lon: string;
+  Province: string;
+  Status: string;
+}
+
+export type CountrySummaryResonse = CountrySummaryInfo[];
